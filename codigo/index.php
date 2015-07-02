b9bf6ffe47ae2aab8eb54e8645fdcd1e1d697f3c
Sebastián <sberlati@gmail.com>
2015-07-02 00:27:34 -0300

    Comenzando con el diseño

--- codigo/index.php
+++ codigo/index.php
@@ -1,9 +1,36 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<link rel="icon" href="../../favicon.ico">
+		<meta charset="utf-8">		
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		
 		<title>Compra en línea</title>
+		
+		<link href="/css/bootstrap.min.js" rel="stylesheet">
 	</head>
 	<body>
+		<div class="container"><!-- Contenedor principal -->
 		
+			<!-- Cabecera de la página -->
+			<div class="header clearfix">
+			<!-- Navegador -->
+				<nav>
+					<ul class="nav nav-pills pull-right">
+						<li role="presentation" class="active"><a href="#">Página principal</a></li>
+					</ul>
+				</nav>
+			<!-- /Navegador -->
+			
+			<h3 class="text-muted">Compra en línea</h3>
+			</div>
+			<!-- /Cabecera de la página -->
+			
+			
+			
+		</div><!-- /Contenedor principal -->
 	</body>
 </html>
