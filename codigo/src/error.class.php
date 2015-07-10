13f98fb90c74f9960e10488987889e420d5c0253
Sebastián <sberlati@gmail.com>
2015-07-10 19:59:39 -0300

    error class comentada
    Se comentaron los métodos explicando qué hace cada uno, y se agregó un
    link de ayuda acerca de las propiedades de un objeto de excepción.

--- codigo/src/error.class.php
+++ codigo/src/error.class.php
@@ -13,6 +13,13 @@ class error {
 	private static $enable_log_writing	 = false;				// Habilitar o no la escritura de errores en el log
 	private static $log_filename		 = '/error_log.txt';	// Nombre del archivo donde se graban los mensajes
 	
+	/**
+	* Constructor de la clase que recibe básicamente lo que contiene el objeto
+	* de la excepción. 
+	* 
+	* Para obtener un ejemplo de los métodos que tiene un objeto de excepción, podemos revisar
+	* el siguiente link: http://php.net/manual/es/language.exceptions.php#91159
+	*/
 	public function __construct($message, $code, $file, $line, $critical = $self::$critical_default) {
 		if(self::$enable_log_writing)
 			$this->writeLogEntry($message, $code, $file, $line);
@@ -20,11 +27,21 @@ class error {
 			$this->writeErrorPage($message);
 	}
 	
+	/**
+	* Escribe un mensaje en pantalla para el usuario. Es imporante que se evite
+	* dar información de más al usuario final sobre el error, así como evitar que
+	* se muestren nombres de los archivos del sistema, etc
+	*/
 	private function writeErrorPage($message) {
 		echo '<h3>Ha ocurrido un error</h3><br><br>' . $message . '<br>Lamentamos las molestias. ';
 		if(self::$enable_log_writing) echo 'Se ha enviado un informe y el equipo lo leerá en breve.';
 	}
 	
+	/**
+	* Si la propiedad $enable_log_writing es TRUE, se procede a escribir en el log
+	* (definido en la propiedad $log_filename) información de la excepción de forma
+	* detallada para su revisión
+	*/
 	private function writeLogEntry($message, $code, $file, $line) {
 		try {
 			$logMessage = '[' . date('d/m/Y H:i:s') . '] Cód ' . $code . ' en ' . $file . ' línea ' . $line . ' - ' . $message . PHP_EOL;
